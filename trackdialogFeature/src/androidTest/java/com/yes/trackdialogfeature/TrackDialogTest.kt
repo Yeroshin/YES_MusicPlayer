0bc673c0502146195675cdf1056ee07ebcd9775d
Stanislav Yeroshin <twister2004@inbox.ru>
2023-08-14 10:27:51 +0300

    TrackDialogTest all tests passed 14.08.2023

--- trackdialogFeature/src/androidTest/java/com/yes/trackdialogfeature/TrackDialogTest.kt
+++ trackdialogFeature/src/androidTest/java/com/yes/trackdialogfeature/TrackDialogTest.kt
@@ -8,7 +8,9 @@ import androidx.fragment.app.testing.launchFragment
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.room.util.copy
 import androidx.test.espresso.IdlingRegistry
+import com.example.shared_test.UiFixturesGenerator
 import com.yes.trackdialogfeature.domain.entity.Menu
 import com.yes.trackdialogfeature.domain.usecase.GetMenuUseCase
 import com.yes.trackdialogfeature.domain.usecase.SaveTracksToPlaylistUseCase
@@ -25,9 +27,12 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.test.StandardTestDispatcher
 import kotlinx.coroutines.test.TestDispatcher
+import kotlinx.coroutines.test.TestScope
 import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runTest
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
@@ -59,7 +64,7 @@ class TrackDialogTest {
             )
         }
 
-         private val _uiState: MutableStateFlow<TrackDialogContract.State> = MutableStateFlow(
+        private val _uiState: MutableStateFlow<TrackDialogContract.State> = MutableStateFlow(
             TrackDialogContract.State(
                 TrackDialogContract.TrackDialogState.Idle
             )
@@ -73,15 +78,20 @@ class TrackDialogTest {
              scope.launch {
                  _uiState.emit(state)
              }*/
-       /* fun pushEvent(reduce: TrackDialogContract.State.() -> TrackDialogContract.State) {
+        /* fun pushEvent(reduce: TrackDialogContract.State.() -> TrackDialogContract.State) {
             EspressoIdlingResource.increment()
             val newState = currentState.reduce()
             _uiState.value = newState
             EspressoIdlingResource.decrement()
         }*/
-        fun pushEvent(newState: TrackDialogContract.State) {
+        @OptIn(ExperimentalCoroutinesApi::class)
+        fun pushEvent(reduce: TrackDialogContract.State.() -> TrackDialogContract.State) = runTest {
 
-            _uiState.value = newState
+            val newState = currentState.reduce()
+            launch {
+                _uiState.value = newState
+            }
+
 
         }
 
@@ -142,48 +152,41 @@ class TrackDialogTest {
 
     @Before
     fun setUp() {
-      //  Dispatchers.setMain(testDispatcher)
+        //  Dispatchers.setMain(testDispatcher)
         MockKAnnotations.init(this, relaxUnitFun = true) // turn relaxUnitFun on for all mocks
         IdlingRegistry.getInstance().register(EspressoIdlingResource.countingIdlingResource)
         scenario = launchFragment(
             factory = trackDialogFactory,
-                  //  initialState = Lifecycle.State.STARTED
+            //  initialState = Lifecycle.State.STARTED
         )
 
     }
 
     @After
     fun tearDown() {
-        Dispatchers.resetMain()
+        //  Dispatchers.resetMain()
         scenario.close()
     }
 
     @Test
     fun onInitShowsTrackDialogStateIdle() {
-       /* scenario.onFragment { fragment ->
+        /* scenario.onFragment { fragment ->
             assert(fragment.requireDialog().isShowing)
             IdlingRegistry.getInstance().unregister(EspressoIdlingResource.countingIdlingResource)
 
         }*/
-       // scenario.moveToState(Lifecycle.State.STARTED)
+        // scenario.moveToState(Lifecycle.State.STARTED)
         trackDialog {
             matchTitleHasNoText()
             matchProgressBarIsNotDisplayed()
             matchDisableViewIsNotDisplayed()
         }
-        viewModel.pushEvent (
-            TrackDialogContract.State(
-                TrackDialogContract.TrackDialogState.Loading
+
+        viewModel.pushEvent {
+            copy(
+                trackDialogState = TrackDialogContract.TrackDialogState.Loading
             )
-
-        )
-
-
-      /*  trackDialog {
-            matchTitleHasNoText()
-            matchProgressBarIsNotDisplayed()
-            matchDisableViewIsNotDisplayed()
-        }*/
+        }
 
         trackDialog {
             matchProgressBarDisplayed()
@@ -192,14 +195,14 @@ class TrackDialogTest {
 
     }
 
-  /*  @Test
+    @Test
     fun loading() {
 
-        /*  viewModel.pushEvent(
-              TrackDialogContract.State(
-                  TrackDialogContract.TrackDialogState.Loading
-              )
-          )*/
+        viewModel.pushEvent {
+            copy(
+                TrackDialogContract.TrackDialogState.Loading
+            )
+        }
 
         trackDialog {
             matchTitleHasNoText()
@@ -210,22 +213,15 @@ class TrackDialogTest {
 
     @Test
     fun dataSuccess() {
-        scenario.onFragment {
-            /*  viewModel.pushEvent(
-                  TrackDialogContract.State(
-                      TrackDialogContract.TrackDialogState.Loading
-                  )
-              )*/
-        }
         val number = 200
         val item = UiFixturesGenerator.generateArtistsMenuUi(number)
-        /* viewModel.pushEvent(
-             TrackDialogContract.State(
-                 TrackDialogContract.TrackDialogState.Success(
-                     item
-                 )
-             )
-         )*/
+        viewModel.pushEvent {
+            copy(
+                TrackDialogContract.TrackDialogState.Success(
+                    item
+                )
+            )
+        }
         trackDialog {
             matchTitleText(item.title)
             matchProgressBarIsNotDisplayed()
@@ -235,68 +231,68 @@ class TrackDialogTest {
                 item.items[number - 1]
             )
         }
+    }
+}
 
+//////////////////////worked
+/*    onView(withId(com.yes.coreui.R.id.recyclerView)).perform(
+        scrollToPosition<TrackDialogAdapter.TrackHolder>(
+            0
+        )
+    )
 
-        //////////////////////worked
-        /*    onView(withId(com.yes.coreui.R.id.recyclerView)).perform(
-                scrollToPosition<TrackDialogAdapter.TrackHolder>(
-                    0
-                )
-            )
+    onView(withText("Artist")).check(matches(isDisplayed()))
+    //onView(withText("Description")).check(matches(isDisplayed()))
+    //////////////////////////////
 
-            onView(withText("Artist")).check(matches(isDisplayed()))
-            //onView(withText("Description")).check(matches(isDisplayed()))
-            //////////////////////////////
+    onView(withId(com.yes.coreui.R.id.recyclerView)) // Replace 'recycler_view' with the ID of your RecyclerView
+        .check(matches(atPosition(0, R.id.item_title, "Album")))
+*/
+//////////////////////////////
 
-            onView(withId(com.yes.coreui.R.id.recyclerView)) // Replace 'recycler_view' with the ID of your RecyclerView
-                .check(matches(atPosition(0, R.id.item_title, "Album")))
-    */
-        //////////////////////////////
-    }*/
+/* private fun atPosition(position: Int, targetViewId: Int, expectedText: String): Matcher<View> {
+     return object : BoundedMatcher<View, RecyclerView>(RecyclerView::class.java) {
+         override fun describeTo(description: Description) {
+             //description.appendText("has item at position $position: ")
 
-    /* private fun atPosition(position: Int, targetViewId: Int, expectedText: String): Matcher<View> {
-         return object : BoundedMatcher<View, RecyclerView>(RecyclerView::class.java) {
-             override fun describeTo(description: Description) {
-                 //description.appendText("has item at position $position: ")
-
-             }
-
-             override fun matchesSafely(recyclerView: RecyclerView): Boolean {
-                 val viewHolder = recyclerView.findViewHolderForAdapterPosition(position)
-                 val targetView = viewHolder?.itemView?.findViewById<TextView>(targetViewId)
-
-                 return targetView?.text == expectedText
-             }
          }
-     }*/
-    /*
-    private fun atPosition(position: Int, itemMatcher: Matcher<View>, expectedText: String?): Matcher<View> {
-        checkNotNull(itemMatcher)
-        return object : BoundedMatcher<View, RecyclerView>(RecyclerView::class.java) {
-            override fun describeTo(description: Description) {
-                description.appendText("has item at position $position: ")
-                itemMatcher.describeTo(description)
+
+         override fun matchesSafely(recyclerView: RecyclerView): Boolean {
+             val viewHolder = recyclerView.findViewHolderForAdapterPosition(position)
+             val targetView = viewHolder?.itemView?.findViewById<TextView>(targetViewId)
+
+             return targetView?.text == expectedText
+         }
+     }
+ }*/
+/*
+private fun atPosition(position: Int, itemMatcher: Matcher<View>, expectedText: String?): Matcher<View> {
+    checkNotNull(itemMatcher)
+    return object : BoundedMatcher<View, RecyclerView>(RecyclerView::class.java) {
+        override fun describeTo(description: Description) {
+            description.appendText("has item at position $position: ")
+            itemMatcher.describeTo(description)
+        }
+
+        override fun matchesSafely(view: RecyclerView): Boolean {
+            val viewHolder = view.findViewHolderForAdapterPosition(position)
+            if (viewHolder == null) {
+                // has no item on such position
+                return false
+            }
+            val itemView = viewHolder.itemView
+            var matchResult = itemMatcher.matches(itemView)
+
+            if (expectedText != null) {
+                val textView = itemView.findViewById<TextView>(R.id.item_title)
+                val actualText = textView.text.toString()
+                matchResult = matchResult && actualText == expectedText
             }
 
-            override fun matchesSafely(view: RecyclerView): Boolean {
-                val viewHolder = view.findViewHolderForAdapterPosition(position)
-                if (viewHolder == null) {
-                    // has no item on such position
-                    return false
-                }
-                val itemView = viewHolder.itemView
-                var matchResult = itemMatcher.matches(itemView)
-
-                if (expectedText != null) {
-                    val textView = itemView.findViewById<TextView>(R.id.item_title)
-                    val actualText = textView.text.toString()
-                    matchResult = matchResult && actualText == expectedText
-                }
-
-                return matchResult
-            }
+            return matchResult
         }
     }
+}
 */
 
-}
+
