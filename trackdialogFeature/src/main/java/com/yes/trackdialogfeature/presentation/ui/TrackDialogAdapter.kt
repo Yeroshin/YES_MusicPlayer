b9c7d5ee162ca4c0b1a4830be5aa5ff1da6e879d
Stanislav Yeroshin <twister2004@inbox.ru>
2023-09-16 16:22:43 +0300

    track dialog adapter bug fixed 16.09.2023

--- trackdialogFeature/src/main/java/com/yes/trackdialogfeature/presentation/ui/TrackDialogAdapter.kt
+++ trackdialogFeature/src/main/java/com/yes/trackdialogfeature/presentation/ui/TrackDialogAdapter.kt
@@ -2,6 +2,7 @@ package com.yes.trackdialogfeature.presentation.ui
 
 import android.view.LayoutInflater
 import android.view.View.GONE
+import android.view.View.VISIBLE
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.yes.trackdialogfeature.databinding.ItemMediaBinding
@@ -9,23 +10,15 @@ import com.yes.trackdialogfeature.presentation.model.MenuUi
 
 class TrackDialogAdapter :
     RecyclerView.Adapter<TrackDialogAdapter.TrackHolder>() {
-    // private lateinit var viewModel: TrackDialogViewModel
     private var mediaList = listOf<MenuUi.ItemUi>()
-
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TrackHolder {
         val binding = ItemMediaBinding
             .inflate(LayoutInflater.from(parent.context), parent, false)
-
         return TrackHolder(binding)
     }
 
     override fun onBindViewHolder(holder: TrackHolder, position: Int) {
-
-        /* when(menu.type){
-             "Media.TITLE"->iconType=2
-             else->1
-         }*/
         holder.bind(position, mediaList[position], mediaList[position].iconType)
     }
 
@@ -33,22 +26,13 @@ class TrackDialogAdapter :
         return mediaList.size
     }
 
-    /* fun setViewModel(vm: TrackDialogViewModel) {
-         this.viewModel = vm
-     }*/
-
     fun setItems(items: List<MenuUi.ItemUi>) {
-
         mediaList = items
         notifyDataSetChanged()
     }
 
     fun getItems(): List<MenuUi.ItemUi> {
         return mediaList
-    }
-
-    fun onCheckboxClicked(position: Int) {
-
     }
 
     inner class TrackHolder( val binding: ItemMediaBinding) :
@@ -62,9 +46,11 @@ class TrackDialogAdapter :
             binding.root.isSelected = false
             binding.root.isActivated = false
             binding.itemTitle.text = item.name
-            //TO DO think about refactor this
+            //TODO think about how to refactor this
             if(item.name==".."){
                 binding.checkBox.visibility=GONE
+            }else{
+                binding.checkBox.visibility= VISIBLE
             }
             binding.checkBox.setOnCheckedChangeListener{ buttonView, isChecked ->
                 mediaList[position].selected=isChecked
@@ -72,12 +58,6 @@ class TrackDialogAdapter :
             binding.icon.setImageLevel(iconType)
             binding.root.setOnClickListener {
                 mediaList[position].onClick(mediaList[position].param)
-                /*  trackList[position].onClick(
-                      TrackDialogContract.Event.OnItemClicked(
-                          item.id,
-                          item.name
-                      )
-                  )*/
             }
         }
     }
