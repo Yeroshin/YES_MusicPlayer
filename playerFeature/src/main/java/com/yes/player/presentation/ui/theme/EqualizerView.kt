0e63601c5ad643c558dcc72ef86c808e2afb1d33
Stanislav Yeroshin <twister2004@inbox.ru>
2023-11-02 11:06:43 +0300

    compose visualizer view 2.11.2023

--- playerFeature/src/main/java/com/yes/player/presentation/ui/theme/EqualizerView.kt
+++ playerFeature/src/main/java/com/yes/player/presentation/ui/theme/EqualizerView.kt
@@ -4,8 +4,6 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -14,33 +12,38 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.ui.unit.times
+
 @Preview
 @Composable
 fun EqualizerView() {
-    val value1 =100
+    val value1 = 100
     val value2 = 40
-    val value3 =25
+    val value3 = 25
     val value4 = 20
-    val value5 = 2
-    val columnCount = 5
+    val value5 = 60
+    val values = listOf(
+        value1,
+        value2,
+        value3,
+        value4,
+        value5,
+    )
+    val columnCount = values.size
 
-    val maxValue=100
-
-    val maxColumnHeight = listOf(value1, value2, value3, value4, value5).maxOrNull() ?: 0
+    val maxValue = 100
 
     val screenWidth = LocalConfiguration.current.screenWidthDp.dp
     val screenHeight = LocalConfiguration.current.screenHeightDp.dp
-    val squareSize = (screenWidth / columnCount )
-    val columnSpacing = squareSize/8
-    val maxCount=screenHeight/squareSize
-    val count =((value1*maxCount)/maxValue).toInt()
+    var squareSize = (screenWidth / columnCount)
+    val columnSpacing = squareSize / 8
+    squareSize -= columnSpacing
+    val maxHeightCount = screenHeight / (squareSize + columnSpacing)
 
-    val columnHeights = listOf(value1, value2, value3, value4, value5)
-        .map { (it / 100f) * maxColumnHeight }
+
+
     Row(
         modifier = Modifier.fillMaxWidth(),
         horizontalArrangement = Arrangement.spacedBy(columnSpacing)
@@ -48,13 +51,13 @@ fun EqualizerView() {
         repeat(columnCount) { columnIndex ->
             Column(
                 modifier = Modifier.fillMaxHeight(),
-                verticalArrangement = Arrangement.Bottom
+                verticalArrangement = Arrangement.spacedBy(columnSpacing)
             ) {
+                val count = ((values[ columnIndex] * maxHeightCount) / maxValue).toInt()
                 repeat(count) { heightIndex ->
                     Box(
                         modifier = Modifier
-                            .padding(columnSpacing)
-                            .size(squareSize-columnSpacing*2 )
+                            .size(squareSize)
                             .background(Color.White)
                     )
                 }
