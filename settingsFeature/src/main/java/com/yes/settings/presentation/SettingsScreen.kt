ab74849a2cc755d13b3121c5fa70526431b9546e
Stanislav Yeroshin <twister2004@inbox.ru>
2024-04-27 15:24:37 +0300

    add: settings module 27.04.2023

---
+++ settingsFeature/src/main/java/com/yes/settings/presentation/SettingsScreen.kt
@@ -0,0 +1,29 @@
+package com.yes.settings.presentation
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.viewbinding.ViewBinding
+import com.yes.core.presentation.BaseDependency
+import com.yes.core.presentation.BaseFragment
+import com.yes.core.presentation.UiState
+
+class SettingsScreen: BaseFragment() {
+    override val dependency: BaseDependency
+        get() = TODO("Not yet implemented")
+
+    override fun createBinding(inflater: LayoutInflater, container: ViewGroup?): ViewBinding {
+        TODO("Not yet implemented")
+    }
+
+    override fun setUpView() {
+        TODO("Not yet implemented")
+    }
+
+    override fun renderUiState(state: UiState) {
+        TODO("Not yet implemented")
+    }
+
+    override fun showEffect() {
+        TODO("Not yet implemented")
+    }
+}
